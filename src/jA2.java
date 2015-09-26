2925a50f401bf5fdc195837f3ca4619dd09a3b9b
meta_system <meta_system@online.de>
2015-09-26 10:51:55 +0200

    Added Assignment sheet

---
+++ src/jA2.java
@@ -0,0 +1,16 @@
+
+public class jA2 {
+
+	String fString = "000000000"
+			+ "011011110"
+			+ "011010110"
+			+ "011110110"
+			+ "011110110"
+			+ "000000000";
+
+	public static void main(String[] args) {
+		boolean[][] fArray = new boolean{
+
+	}
+
+}
