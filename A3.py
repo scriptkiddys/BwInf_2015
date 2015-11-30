a4c4021506bea5a97685ac115a9c32edb89f8f70
karwel <karl.welzel@gmail.com>
2015-11-30 21:59:31 +0100

    last commit

--- A3.py
+++ A3.py
@@ -1,16 +1,20 @@
-def ways_to_arrange(number_of_bottles, buckets):
-    print(number_of_bottles, buckets)
+from functools import lru_cache
+
+@lru_cache(None)
+def ways_to_arrange(number_of_bottles, buckets): 
     if number_of_bottles < 0 or number_of_bottles > sum(buckets):
         return 0
-    if not buckets:
-        if number_of_bottles == 0:
-            return 1
-        else:
-            return 0
-    bucket = buckets[0]
-    return sum([ways_to_arrange(number_of_bottles-filllevel, buckets[1:]) for filllevel in range(bucket+1)])
+    elif number_of_bottles == 0:
+        return 1
+    elif len(buckets) == 1 and number_of_bottles <= buckets[0]:
+        return 1
+    elif not buckets: #number_of_bottles > 0
+        return 0
+    else:
+        bucket = buckets[0]
+        return sum([ways_to_arrange(number_of_bottles-fillnumber, buckets[1:]) for fillnumber in range(min(bucket, number_of_bottles)+1)])
 
-with open(r".\Flaschenzug\flaschenzug3.txt") as d:
+with open(r".\Flaschenzug\flaschenzug0.txt") as d:
     STRING = d.read()
     while STRING.rsplit("\n", 1)[1] == "": #leere letzte Zeile löschen
         STRING = STRING.rsplit("\n", 1)[0]
@@ -18,8 +22,10 @@ with open(r".\Flaschenzug\flaschenzug3.txt") as d:
 lines = STRING.split("\n")
 
 number_of_bottles = int(lines[0])
-buckets = sorted([int(b) for b in lines[2].split(" ")], reverse=True)
-print(number_of_bottles)
-print(buckets)
+buckets = [int(b) for b in lines[2].split(" ")]
 
-print(ways_to_arrange(number_of_bottles, buckets))
+##print(number_of_bottles)
+##print(buckets)
+##print()
+print(ways_to_arrange(number_of_bottles, tuple(buckets)), "Moeglichkeiten")
+
